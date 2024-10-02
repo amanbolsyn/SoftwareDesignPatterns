7a44c04242be26be9447df956b77a2e14b3652c9
Aman Nurgozhiyev <nurgozhiyevaman@gmail.com>
2024-10-02 15:05:03 +0500

    Create Driver.java

---
+++ Driver.java
@@ -0,0 +1,53 @@
+public class Driver {
+    private String name;
+    private String carModel;
+    private String licenseNumber;
+    private int experienceYears;
+    private boolean isAvailable;
+
+    public static class DriverBuilder{
+        private String name;
+        private String carModel;
+        private String licenseNumber;
+        private int experienceYears;
+        private boolean isAvailable;
+        public DriverBuilder withName(String name){
+            this.name = name;
+            return this;
+        }
+
+      //допишите код
+        public DriverBuilder withCarModel(String carModel){
+            this.carModel = carModel;
+            return this;
+        }
+        public DriverBuilder withLicenseNumber(String licenseNumber){
+            this.licenseNumber = licenseNumber;
+            return this;
+        }
+        public DriverBuilder withExperienceYears(int experienceYears){
+            this.experienceYears = experienceYears;
+            return this;
+        }
+        public DriverBuilder isAvailable(boolean isAvailable){
+            this.isAvailable = isAvailable;
+            return this;
+        }
+        public Driver build(){
+            return new Driver(this);
+        }
+    }
+
+    private Driver(DriverBuilder builder) {
+        this.name = builder.name;
+        this.carModel = builder.carModel;
+        this.licenseNumber = builder.licenseNumber;
+        this.experienceYears = builder.experienceYears;
+        this.isAvailable = builder.isAvailable;
+    }
+
+    @Override
+    public String toString() {
+        return "Driver name = " + name + ",\nCar model = " + carModel + ",\nLicense number =" + licenseNumber + ",\nYears of experience = " + experienceYears + (isAvailable ? ",\nAvailable" : ",\nNot Available");
+    }
+}
