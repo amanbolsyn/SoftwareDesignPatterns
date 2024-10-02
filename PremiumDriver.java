6d6c94b8c34dd27b8882db7633c2e5fb5ef36a74
Aman Nurgozhiyev <nurgozhiyevaman@gmail.com>
2024-10-02 15:17:05 +0500

    Create PremiumDriver.java

---
+++ PremiumDriver.java
@@ -0,0 +1,10 @@
+public class PremiumDriver extends Driver {
+   public PremiumDriver(String driverName) {
+        super(driverName);
+    }
+
+    @Override
+    public void drive() {
+        System.out.println("Driver with comfort");
+    }
+}
