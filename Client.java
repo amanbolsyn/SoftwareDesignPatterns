722fd413b7be73399c6a1cb7b857fc89dfa0cb73
Aman Nurgozhiyev <nurgozhiyevaman@gmail.com>
2024-10-02 19:42:34 +0500

    Create Client.java

---
+++ Client.java
@@ -0,0 +1,10 @@
+public static void main(String[] args) { 
+// Passenger passenger = new Passenger();
+
+DriverManager driverManager = DriverManager.getInstance();
+
+String driver = driverManager.findNearestDriver("Location A"); 
+
+// passenger.assignTaxi(driver);
+
+}
